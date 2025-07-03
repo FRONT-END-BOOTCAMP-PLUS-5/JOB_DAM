a9a9c410a3c9a0fa65c59797e97caee7066c2f03
doyoung <gustmdehdud@gmail.com>
2025-07-03 10:05:21 +0900

    클린 아키텍처 arrow function 에서 class 형식으로 변경

--- app/(backend)/api/category/route.ts
+++ app/(backend)/api/category/route.ts
@@ -6,8 +6,8 @@ import { createClient } from "@/app/utils/supabase/server";
 
 export async function GET() {
   const supabase: SupabaseClient = await createClient(); // supabse 불러오기
-  const categoryRepository = SbCategoryRepository(supabase); // 쿼리로 supabase 데이터 불러오기
-  const getCategoryUsecase = GetCategoryListUsecase(categoryRepository); // 가져온 데이터 가공
+  const categoryRepository = new SbCategoryRepository(supabase); // 쿼리로 supabase 데이터 불러오기
+  const getCategoryUsecase = new GetCategoryListUsecase(categoryRepository); // 가져온 데이터 가공
   const categories = await getCategoryUsecase.execute(); // 가공된 데이터 반환
 
   return NextResponse.json(categories);
--- app/(backend)/repositories/SbCategoryRepository.ts
+++ app/(backend)/repositories/SbCategoryRepository.ts
@@ -1,47 +1,29 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 import { Category } from "../domain/entities/Category";
 import { CategoryTable } from "../domain/tables/CategoryTable";
+import { CategoryRepository } from "../domain/repositories/CategoryRepository";
 
-export const SbCategoryRepository = (supabase: SupabaseClient) => {
+export class SbCategoryRepository implements CategoryRepository {
+  private supabase;
+
+  constructor(supabase: SupabaseClient) {
+    this.supabase = supabase;
+  }
+
   // 데이터베이스 데이터를 도메인 엔티티로 변환
-  const mapToMenu = (category: CategoryTable): Category => ({
-    id: category.id,
-    name: category.name,
-  });
+  private static mapToMenu(category: CategoryTable): Category {
+    return {
+      id: category.id,
+      name: category.name,
+    };
+  }
 
-  const findAll = async () => {
-    const { data, error } = await supabase.from("category").select();
+  async findAll(): Promise<Category[]> {
+    const { data, error } = await this.supabase.from("category").select();
 
     if (error) throw new Error(error.message);
-    return data.map((menu) => mapToMenu(menu)) as Category[];
-  };
-
-  return {
-    findAll,
-  };
-};
-
-// export class SbCategoryRepository implements CategoryRepository {
-//   private supabase;
-
-//   constructor(supabase: SupabaseClient) {
-//     this.supabase = supabase;
-//   }
-
-//   // 데이터베이스 데이터를 도메인 엔티티로 변환
-//   private static mapToMenu(category: CategoryTable): Category {
-//     return {
-//       id: category.id,
-//       name: category.name,
-//     };
-//   }
-
-//   async findAll(): Promise<Category[]> {
-//     const { data, error } = await this.supabase.from("category").select();
-
-//     if (error) throw new Error(error.message);
-//     return data.map((menu) =>
-//       SbCategoryRepository.mapToMenu(menu)
-//     ) as Category[];
-//   }
-// }
+    return data.map((menu) =>
+      SbCategoryRepository.mapToMenu(menu)
+    ) as Category[];
+  }
+}
