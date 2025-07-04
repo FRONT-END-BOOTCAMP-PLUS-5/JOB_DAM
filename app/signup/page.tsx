beb0820c254121043da9be5560d6e51ae2aefc63
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2025-07-04 16:38:18 +0900

    회원가입 헤더 인풋 중간 작업

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -13,11 +13,24 @@ export default function SignupPage() {
   const { register, handleSubmit } = useForm<FormInput>();
 
   return (
-    <div className={styles.signup_page_container}>
+    <header className={styles.signup_page_container}>
       <div className={styles.signup_title_container}>
         <h1 className={styles.signup_title_text}>회원가입</h1>
         <p>당신의 커리어 여정을 함께 나아가세요</p>
       </div>
-    </div>
+
+      <section className={styles.signup_image_container}>
+        <div className={styles.signup_image_container_item}>
+          <Image src={'/images/userImage.svg'} alt="유저 이미지" width={50} height={50} />
+          <Input
+            name="nickname"
+            label="닉네임"
+            required={true}
+            placeholder="닉네임을 입력해주세요"
+            className={styles.signup_form_item}
+          />
+        </div>
+      </section>
+    </header>
   );
 }
