c9dd6a1213c9336ed50fcf8ccd8a01d394c06aaa
doyoung <gustmdehdud@gmail.com>
2025-07-04 16:43:56 +0900

    마이페이지 멘토신청하기 페이지 추가

---
+++ app/(anon)/mypage/application/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import styles from './application.module.scss';
+
+const Application = () => {
+  return <section className={styles.user_container}>asdf</section>;
+};
+
+export default Application;
