61f35db6c82171bfd36d5b5fd99093cb36b954c7
doyoung <gustmdehdud@gmail.com>
2025-07-04 17:04:01 +0900

    멘토 신청 페이지 추가

--- app/(anon)/mypage/application/page.tsx
+++ app/(anon)/mypage/application/page.tsx
@@ -3,7 +3,26 @@
 import styles from './application.module.scss';
 
 const Application = () => {
-  return <section className={styles.user_container}>asdf</section>;
+  return (
+    <section className={styles.user_container}>
+      <h3>멘토 신청하기</h3>
+      <div className={styles.input_box}>
+        <h4>회사이름</h4>
+        <input type="text" placeholder="회사이름" />
+      </div>
+      <div className={styles.input_box}>
+        <h4>직급</h4>
+        <input type="text" placeholder="직급" />
+      </div>
+      <div className={styles.input_box}>
+        <h4>근무기간</h4>
+        <div>
+          <input type="date" />~
+          <input type="date" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Application;
