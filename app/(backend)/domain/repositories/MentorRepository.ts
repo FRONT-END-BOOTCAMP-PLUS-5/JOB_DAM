0185b93b4fedd8418330778179414ce653b6e94f
doyoung <gustmdehdud@gmail.com>
2025-07-07 16:01:26 +0900

    멘토 신청, 멘토 신청서 가져오기 api 추가

--- app/(backend)/domain/repositories/MentorRepository.ts
+++ app/(backend)/domain/repositories/MentorRepository.ts
@@ -2,4 +2,6 @@ import { Mentor } from '../entities/Mentor';
 
 export interface MentorRepository {
   insertMentor(): Promise<Mentor>;
+  findAll(): Promise<Mentor[]>;
+  findOne(member_id: string): Promise<Mentor>;
 }
