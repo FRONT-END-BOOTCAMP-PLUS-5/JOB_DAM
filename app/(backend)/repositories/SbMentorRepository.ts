0072eda8761dc6be54deca6a8cf3ac8716d8d50e
doyoung <gustmdehdud@gmail.com>
2025-07-07 16:02:21 +0900

    멘토 신청관련 DB 쿼리 추가

--- app/(backend)/repositories/SbMentorRepository.ts
+++ app/(backend)/repositories/SbMentorRepository.ts
@@ -3,10 +3,12 @@ import { Mentor } from '../domain/entities/Mentor';
 import { MentorRepository } from '../domain/repositories/MentorRepository';
 
 interface ClientProp {
-  title?: string;
-  content?: string;
-  category_id?: number;
+  company?: string;
+  level?: string;
+  work_period?: string;
 }
+
+const MENTOR_TABLE_NAME = 'mentor_application';
 
 export class SbMentorRepository implements MentorRepository {
   private supabase;
@@ -18,9 +20,27 @@ export class SbMentorRepository implements MentorRepository {
   }
 
   async insertMentor(): Promise<Mentor> {
-    const { data, error } = await this.supabase.from('mentor').insert([this.clientData]).select('*').single();
+    const { data, error } = await this.supabase
+      .from(MENTOR_TABLE_NAME)
+      .insert([{ ...this.clientData, member_id: '0bd61fbf-71fd-44e1-a590-1e53af363c3c' }])
+      .select('*')
+      .single();
+    if (error) throw new Error(error.message);
+    return data as Mentor;
+  }
+
+  async findAll(): Promise<Mentor[]> {
+    const { data, error } = await this.supabase.from(MENTOR_TABLE_NAME).select('*');
 
     if (error) throw new Error(error.message);
+    return data as Mentor[];
+  }
+
+  async findOne(member_id: string): Promise<Mentor> {
+    const { data, error } = await this.supabase.from(MENTOR_TABLE_NAME).select().eq('member_id', member_id).single();
+
+    if (error) throw new Error(error.message);
+
     return data as Mentor;
   }
 }
