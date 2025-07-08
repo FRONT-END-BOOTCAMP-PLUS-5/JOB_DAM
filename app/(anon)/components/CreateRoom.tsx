a06aef24c3a9d5fcf57ea726dc0f3f7d1488a494
printseungjoo <printseungjoo@gmail.com>
2025-07-08 12:22:45 +0900

    방 만들기 페이지 UI 수정

--- app/(anon)/components/CreateRoom.tsx
+++ app/(anon)/components/CreateRoom.tsx
@@ -18,15 +18,21 @@ const CreateRoom = () => {
                 <h2 className={styles.createRoomTitle}> 방 만들기 </h2>
                 <p className={styles.createRoomDescription}> 당신의 가치를 공유하세요! </p>
                 <p className={styles.createRoomOption}> 방 이름 </p>
-                <input onChange={(e)=>setRoomName(e.target.value)} className={styles.formStyle} ></input>
+                <input onChange={(e) => setRoomName(e.target.value)} className={styles.formStyle}></input>
                 <p className={styles.createRoomOption}> 방 설명 </p>
                 <textarea className={styles.createTextarea} rows="4" cols="50"></textarea>
                 <p className={styles.createRoomOption}> 멘토 설명 </p>
-                <input onChange={(e)=>setMentorDescription(e.target.value)} className={styles.formStyle}></input>
-                <p className={styles.createRoomOption}> 최대 멘티 수용 인원 </p>
-                <input type="number" className={styles.formStyle}></input>
-                <button className={styles.submitButton}> 방 만들기 </button>
-                <button className={styles.submitButton}> 뒤로 가기 </button>
+                <input onChange={(e) => setMentorDescription(e.target.value)} className={styles.formStyle}></input>
+                <div className={styles.maxNumDiv}>
+                    <p className={styles.createRoomOption}> 최대 멘티 수용 인원 </p>
+                    <input type="number" className={styles.smallFormStyle}></input>
+                </div>
+                <button className={styles.submitButton}> 
+                    <p className={styles.submitButtonP}> 방 만들기 </p> 
+                </button>
+                <button className={styles.submitButton}> 
+                    <p className={styles.submitButtonP}> 뒤로 가기 </p> 
+                </button>
             </div>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Chatroom } from "../app/(anon)/components/Chatroom";
 import { CreateRoom } from "../app/(anon)/components/CreateRoom";
 
 export default function Home() {
-  return(<Chat/>);
+  // return(<Chat/>);
   // return(<Chatroom/>);
-  // return(<CreateRoom/>);
+  return(<CreateRoom/>);
 }
