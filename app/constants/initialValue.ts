d55c132d6bc9d10c401afb1097cfa64604b05dac
doyoung <gustmdehdud@gmail.com>
2025-07-09 17:32:45 +0900

    채팅룸 초기 값 분리

---
+++ app/constants/initialValue.ts
@@ -0,0 +1,16 @@
+export const ChatRoomValue = {
+  id: 0,
+  createdAt: '',
+  title: '',
+  chatMember: [
+    {
+      chatRoomId: 0,
+      memberId: '',
+    },
+  ],
+  createMember: {
+    id: 0,
+    name: '',
+    email: '',
+  },
+};
