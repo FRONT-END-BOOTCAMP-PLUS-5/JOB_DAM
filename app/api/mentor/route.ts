8cd15a14e15452bc4c06e4e9a740718c75ade149
devdongwoo <gimdongwoo64@gmail.com>
2025-07-10 15:50:34 +0900

    폴더 구조 정리, lint, build 정리

---
+++ app/api/mentor/route.ts
@@ -0,0 +1,43 @@
+import { createClient } from '@/app/utils/supabase/server';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { NextRequest, NextResponse } from 'next/server';
+import { SbMentorRepository } from '../../../backend/repositories/SbMentorRepository';
+import { GetMentorUseCase } from '../../../backend/application/usecases/mypage/GetMentorUseCase';
+import { CreateMentorUseCase } from '../../../backend/application/usecases/mypage/CreateMentorUseCase';
+import { GetOneMentorUseCase } from '../../../backend/application/usecases/mypage/GetOneMentorUseCase';
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+
+    const supabase: SupabaseClient = await createClient();
+    const mentorRepository = new SbMentorRepository(supabase, body);
+    const mentor = new CreateMentorUseCase(mentorRepository).create();
+
+    return NextResponse.json({ result: mentor, status: 200 });
+  } catch (err) {
+    if (err instanceof Error) {
+      return NextResponse.json({ message: err.message, status: 503 });
+    }
+  }
+}
+
+export async function GET(request: NextRequest) {
+  const url: URL = new URL(request.url);
+  const param: string = url.searchParams.get('id') || '1';
+
+  const supabase: SupabaseClient = await createClient();
+  const mentorRepository = new SbMentorRepository(supabase);
+
+  let mentor;
+
+  if (param !== '1') {
+    const getOneMentorUseCase = new GetOneMentorUseCase(mentorRepository);
+    mentor = await getOneMentorUseCase.execute(param);
+  } else {
+    const getMentorUseCase = new GetMentorUseCase(mentorRepository);
+    mentor = await getMentorUseCase.execute();
+  }
+
+  return NextResponse.json(mentor);
+}
---
+++ backend/application/usecases/mypage/GetReviewUseCase.ts
@@ -0,0 +1,38 @@
+import { ReviewRepository } from '@/backend/domain/repositories/ReviewRepository';
+import { ReviewDto } from '../../dtos/ReviewDto';
+import { ReviewTable } from '@/backend/domain/tables/ReviewTable';
+
+export class GetReviewUseCase {
+  private repository: ReviewRepository;
+
+  constructor(repository: ReviewRepository) {
+    this.repository = repository;
+  }
+
+  async execute(member_id: string): Promise<{ result: ReviewDto[] }> {
+    const reviewTable: ReviewTable[] = await this.repository.findById(member_id);
+
+    const reviewDto: ReviewDto[] = reviewTable.map((r) => ({
+      id: r.id,
+      chatRoomId: r.chat_room_id,
+      content: r.content,
+      rating: r.rating,
+      createdAt: r.created_at,
+      deletedAt: r.deleted_at,
+      chatRoom: {
+        id: r.chat_room.id,
+        title: r.chat_room.title,
+        description: r.chat_room.description,
+        member: {
+          name: r.chat_room.member.name,
+          nickName: r.chat_room.member.nickname,
+        },
+        createdAt: r.chat_room.created_at,
+      },
+    }));
+
+    return {
+      result: reviewDto,
+    };
+  }
+}
