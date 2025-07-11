12905309995cb70c4779a1be5e9ab6db071b76ae
young <gustmdehdud@gmail.com>
2025-07-12 01:25:13 +0900

    모든 멘토 불러오기 api 추가

---
+++ app/api/mentor/search/route.ts
@@ -0,0 +1,15 @@
+import { createClient } from '@/app/utils/supabase/server';
+import { GetMentorListUseCase } from '@/backend/members/application/usecases/GetMentorListUseCase';
+import { SbMemberRepository } from '@/backend/members/repositories/SbMemberRepository';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const supabase: SupabaseClient = await createClient();
+  const mentorRepository = new SbMemberRepository(supabase);
+
+  const getMentorListUseCase = new GetMentorListUseCase(mentorRepository);
+  const mentor = await getMentorListUseCase.execute();
+
+  return NextResponse.json(mentor);
+}
