f46c1b13e92263cef5934fc34101f0657d36cffd
doyoung <gustmdehdud@gmail.com>
2025-07-14 16:45:00 +0900

    next.config 에 image domains 추가

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  images: {
+    // example.com 아래의 이미지를 Image 컴포넌트에 표시하기 위해 추가
+    domains: ['yqutjsbcupbfpphjmaax.supabase.co'],
+  },
 };
 
 export default nextConfig;
