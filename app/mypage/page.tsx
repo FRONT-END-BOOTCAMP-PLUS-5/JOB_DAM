97ff6e7919071426a8c058f7b597b3760f722040
doyoung <gustmdehdud@gmail.com>
2025-07-14 16:45:49 +0900

    마이페이지 내정보 보기 데이터 연결

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -2,25 +2,36 @@
 
 import Title from '@/app/components/common/Title';
 import styles from './mypage.module.scss';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
+import { useEffect, useState } from 'react';
+import { Member } from '../store/isLogin/loginSlice';
+import Image from 'next/image';
 
 const Mypage = () => {
+  const member = useSelector((state: RootState) => state.login.member);
+
+  const [user, setUser] = useState<Member>(member);
+
+  useEffect(() => {
+    setUser(member);
+  }, [member]);
+
   return (
     <section className={styles.user_container}>
       <Title text="내 정보 보기" />
-      <div className={styles.user_image}>
-        <h5>이미지</h5>
-      </div>
+      <div className={styles.user_image}>{user.img && <Image src={user?.img} alt="" fill />}</div>
       <div className={styles.user_info}>
         <h5>이메일</h5>
-        <p>wkdehdud3@naver.com</p>
+        <p>{user?.email}</p>
       </div>
       <div className={styles.user_info}>
         <h5>이름</h5>
-        <p>장도영</p>
+        <p>{user?.name}</p>
       </div>
       <div className={styles.user_info}>
         <h5>닉네임</h5>
-        <p>그럴수있지</p>
+        <p>{user?.nickname}</p>
       </div>
     </section>
   );
