9cd25b6a4b4552b5cec3784c90dccc0e4cc8330a
printseungjoo <printseungjoo@gmail.com>
2025-07-15 15:52:58 +0900

    시작 화면 개발 완료

---
+++ app/startpage/page.tsx
@@ -0,0 +1,169 @@
+"use client"
+
+import styles from './startpage.module.scss'
+import { createClient } from '@supabase/supabase-js'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
+
+const StartPage = () => {
+    const router = useRouter()
+    const [memberNum, setMemberNum] = useState<number>(0)
+    const [questionNum, setQuestionNum] = useState<number>(0)
+    const [mentorNum, setMentorNum] = useState<number>(0)
+    const [mentorRoomNum, setMentorRoomNum] = useState<number>(0)
+
+    const find_mem_num = async () => {
+        const { data: member_num_data } = await supabase
+            .from('member')
+            .select('id')
+        if (!member_num_data) {
+            return
+        }
+        setMemberNum(member_num_data.length)
+    }
+    find_mem_num()
+
+    const find_question_num = async () => {
+        const { data: question_num_data } = await supabase
+            .from('question')
+            .select('id')
+        if (!question_num_data) {
+            return
+        }
+        setQuestionNum(question_num_data.length)
+    }
+    find_question_num()
+
+    const find_mentor_num = async () => {
+        const { data: mentor_num_data } = await supabase
+            .from('mentor_application')
+            .select('member_id')
+        if (!mentor_num_data) {
+            return
+        }
+        setMentorNum(mentor_num_data.length)
+    }
+    find_mentor_num()
+
+    const find_mentor_room_num = async () => {
+        const { data: mentor_room_num_data } = await supabase
+            .from('chat_room')
+            .select('id')
+        if (!mentor_room_num_data) {
+            return
+        }
+        setMentorRoomNum(mentor_room_num_data.length)
+    }
+    find_mentor_room_num()
+
+    return (
+        <div>
+            <div className={styles.up}>
+                <div className={styles.up_left}>
+                    <h1 className={styles.up_left_h1}> 당신의 커리어 여정을 </h1>
+                    <h1 className={styles.up_left_h1}> 함께 나아가세요 </h1>
+                    <div className={styles.up_left_center}>
+                        <h4 className={styles.up_left_h4}> 전문가 멘토링부터 실무 Q&A까지, </h4>
+                        <h4 className={styles.up_left_h4}> 성장하는 개발자들의 든든한 파트너가 되어드립니다.</h4>
+                    </div>
+                    <div className={styles.up_left_flex}>
+                        <div className={styles.up_left_start}>
+                            🚀 시작하기
+                        </div>
+                        <button className={styles.up_left_go_to_community} onClick={()=>{router.push('/board')}}>
+                            💬 커뮤니티 둘러보기
+                        </button>
+                    </div>
+                </div>
+                <div className={styles.up_right}>
+                    <h3 className={styles.current}> 📊 실시간 현황 </h3>
+                    <div className={styles.current_first_line}>
+                        <div className={styles.current_option}>
+                            <p className={styles.current_number}> {memberNum} </p>
+                            <p className={styles.current_description}> 멤버 수 </p>
+                        </div>
+                        <div className={styles.current_option}>
+                            <p className={styles.current_number}> {questionNum} </p>
+                            <p className={styles.current_description}> 질문 수 </p>
+                        </div>
+                    </div>
+                    <div className={styles.current_first_line}>
+                        <div className={styles.current_option}>
+                            <p className={styles.current_number}>{mentorNum}</p>
+                            <p className={styles.current_description}> 멘토 수 </p>
+                        </div>
+                        <div className={styles.current_option_room_num}>
+                            <p className={styles.current_number}>{mentorRoomNum}</p>
+                            <p className={styles.current_description}> 멘토링 방 수 </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.jobdam_description_div}>
+                <p className={styles.why_jobdam}> 왜 Job담을 선택해야할까요? </p>
+                <p className={styles.jobdam_description}> 개발자의, 개발자에 의한, 개발자를 위한 플랫폼으로 </p>
+                <p className={styles.jobdam_description}> 더 나은 커리어를 만들어가세요 </p>
+            </div>
+            <div className={styles.six_boxes}>
+                <div className={styles.up_three_boxes}>
+                    <div className={styles.first_box}>
+                        <div className={styles.first_box_logo}> 👨🏻‍💻 </div>
+                        <p className={styles.first_box_bold_p}> 전문가 멘토링 </p>
+                        <p className={styles.first_box_p}> 현업에서 활동하는 시니어 개발자들과 1:1 멘토</p>
+                        <p className={styles.first_box_p}> 링을 통해 실무 경험과 커리어 인사이트를 얻으 </p>
+                        <p className={styles.first_box_p}> 세요. </p>
+                    </div>
+                    <div className={styles.first_box}>
+                        <div className={styles.first_box_logo}> 💬 </div>
+                        <p className={styles.first_box_bold_p}> 실시간 Q&A </p>
+                        <p className={styles.first_box_p}> 막힌 부분이 있다면 언제든 질문하세요.커뮤니 </p>
+                        <p className={styles.first_box_p}> 티 전문가들이 신속하고 정확한 답변을 제공합 </p>
+                        <p className={styles.first_box_p}> 니다. </p>
+                    </div>
+                    <div className={styles.first_box}>
+                        <div className={styles.first_box_logo}> 🎯 </div>
+                        <p className={styles.first_box_bold_p}> 맞춤형 성장 로드맵 </p>
+                        <p className={styles.first_box_p}> 개인의 목표와 현재 수준에 맞는 학습 계획을 </p>
+                        <p className={styles.first_box_p}> 세우고, 단계별로 체계적인 성장을 </p>
+                        <p className={styles.first_box_p}>  이뤄나가세요. </p>
+                    </div>
+                </div>
+                <div className={styles.down_three_boxes}>
+                    <div className={styles.first_box}>
+                        <div className={styles.first_box_logo}> 🤝 </div>
+                        <p className={styles.first_box_bold_p}> 네트워킹 </p>
+                        <p className={styles.first_box_p}> 같은 분야의 개발자들과 네트워크를 형성하고, </p>
+                        <p className={styles.first_box_p}> 서로의 경험을 공유하며 함께 성장하세요. </p>
+                    </div>
+                    <div className={styles.first_box}>
+                        <div className={styles.first_box_logo}> 📚 </div>
+                        <p className={styles.first_box_bold_p}> 실무 중심 콘텐츠 </p>
+                        <p className={styles.first_box_p}> 이론보다는 실제 프로젝트에서 사용되는 살아 </p>
+                        <p className={styles.first_box_p}> 있는 지식과 노하우를 배우세요. </p>
+                    </div>
+                    <div className={styles.first_box}>
+                        <div className={styles.first_box_logo}> 🏆 </div>
+                        <p className={styles.first_box_bold_p}> 성과 추적 </p>
+                        <p className={styles.first_box_p}> 학습 진도와 성장 과정을 시각화하여 동기 부여 </p>
+                        <p className={styles.first_box_p}> 를 유지하고 목표를 달성하세요. </p>
+                    </div>
+                </div>
+                <div className={styles.bottom}>
+                    <h1 className={styles.bottom_bold_p}> 지금 바로 시작하세요 </h1>
+                    <p className={styles.bottom_p}> 수천 명의 개발자들이 이미 Job담과 함께 성장하고 있습니다. </p>
+                    <p className={styles.bottom_p}> 당신도 지금 바로 합류해보세요! </p>
+                    <div className={styles.bottom_start_now}>
+                        🚀무료로 시작하기
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { StartPage }
